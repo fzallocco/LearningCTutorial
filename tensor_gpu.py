bc4a41c6c7138bdbf5c82ba56127b459f41f465b
Filippo Zallocco <filizall97@gmail.com>
2024-12-20 01:32:31 +0000

    updated requirements file

--- tensor_gpu.py
+++ tensor_gpu.py
@@ -1,9 +1,11 @@
 import tensorflow as tf
 
+tf.debugging.set_log_device_placement(True)
 gpus = tf.config.list_physical_devices('GPU')
 if gpus:
     print("TensorFlow found the following GPUs:")
     for gpu in gpus:
         print(gpu.name)
+        print("  Memory:", tf.config.experimental.get_memory_info(gpu.name))
 else:
     print("No GPUs found")
